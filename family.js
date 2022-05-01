d2ddb5ec5c569d486bf2752317890c1229a1185a
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-02 00:26:37 +0500

    Update family.js

--- family.js
+++ family.js
@@ -14,3 +14,7 @@ document.write(2 >= 1);
 document.write("<br>");
 document.write(2 <= 4);
 document.write("<br>");
+document.write(null == undefined);
+document.write("<br>");
+document.write(null === undefined);
+document.write("<br>");
