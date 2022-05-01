8bbd51b6e893c62d37d4e8be8ce24a6c8b294373
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-02 00:24:46 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,16 @@
+document.write(2 == 2);
+document.write("<br>");
+document.write(3 === 3);
+document.write("<br>");
+document.write(2 != 1);
+document.write("<br>");
+document.write(2 !== 3);
+document.write("<br>");
+document.write(3 > 2);
+document.write("<br>");
+document.write(2 < 3);
+document.write("<br>");
+document.write(2 >= 1);
+document.write("<br>");
+document.write(2 <= 4);
+document.write("<br>");
